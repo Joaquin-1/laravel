fae3e2b90f91799043d581d71dbb322a609ee937
Joaquin-1 <joaquin.monge@iesdonana.org>
2022-01-13 19:38:14 +0100

    Carga inicial con migr y model

---
+++ app/Models/Articulo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+    use HasFactory;
+
+    public function lineas()
+    {
+
+    }
+
+    public function facturas()
+    {
+
+    }
+
+}
+
+
+
+
